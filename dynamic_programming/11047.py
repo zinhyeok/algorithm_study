27c79329277d4e58c64a436b13d5be9d19e770b3
Jinhyeok Park <86557453+zinhyeok@users.noreply.github.com>
2023-02-06 23:50:16 +0900

    [0206]: 11047, 시간초과 해결

--- dynamic_programming/11047.py
+++ dynamic_programming/11047.py
@@ -1,20 +1,18 @@
 import sys
+
 N, K = map(int, input().split())
 lst_coin = []
-ans_lst = []
 ans = 0
 
 for i in range(N):
     lst_coin.append(int(sys.stdin.readline().strip()))
 
-while True:
-    if K >= lst_coin[-1]:
-        K -= lst_coin[-1]
-        ans_lst.append(lst_coin[-1])
-        ans += 1
-    else:
-        lst_coin.pop()
-    if K==0:
-        break
+lst_coin.sort(reverse=True)
+
+for coin in lst_coin:
+    if K >= coin:
+        ans += K//coin
+        K = K%coin
 
 print(ans)
+
